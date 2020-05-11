381433cfc46b884f24419d40df9b02bbf9c1355e
Cory <me@corylaflamme.com>
2020-05-10 22:48:09 -0400

    Added favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
         <body className="bg-pattern">
           <Main />
           <NextScript />
